028ca6a230d9206e4dbab1ff6de8e9f935409d56
Ankan112 <ankanhalder112@gmail.com>
2022-11-29 20:17:27 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home/Home';
+import Contact from './Pages/Contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: ([
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ])
+  }
+])
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   );
 }
 
---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
+import Navigation from '../Pages/Navigation/Navigation';
+
+const Main = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
