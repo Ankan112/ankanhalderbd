e0f8561288ad7f8612040b09e319dd7398c8d043
Ankan112 <ankanhalder112@gmail.com>
2022-12-17 12:21:46 +0600

    banner section edit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,7 +9,6 @@ const Banner = () => {
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold">Ankan Halder</h1>
                     <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
         </div>
